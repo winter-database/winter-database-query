53f1b11aca511ee3461c4af13bac19d3e94cecc0
changebooks <changebooks@qq.com>
2025-08-03 17:49:48 +0800

    1

--- document/query_filter.sql
+++ document/query_filter.sql
@@ -28,26 +28,26 @@ CREATE TABLE query_filter
 DROP TABLE IF EXISTS query_filter_expression;
 CREATE TABLE query_filter_expression
 (
-    id                 int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-    query_id           int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
-    filter_id          int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
-    expression_code    int(10) unsigned NOT NULL DEFAULT '0' COMMENT '编码',
-    value_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT '子查询',
-    value_name         varchar(64)      NOT NULL DEFAULT '' COMMENT 'Value Name used for REFERENCE PARAMETER',
-    value_string       varchar(255)     NULL     DEFAULT NULL COMMENT 'Value String',
-    value_integer      int(10)          NULL     DEFAULT NULL COMMENT 'Value Integer',
-    value_long         bigint(20)       NULL     DEFAULT NULL COMMENT 'Value Long',
-    value_big_decimal  decimal(14, 3)   NULL     DEFAULT NULL COMMENT 'Value Big Decimal',
-    value_date         datetime         NULL     DEFAULT NULL COMMENT 'Value Date',
-    update_version     int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
-    create_date        datetime         NULL     DEFAULT NULL COMMENT '创建时间',
-    last_update        datetime         NULL     DEFAULT NULL COMMENT '更新时间',
-    internal_recycle   int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
-    internal_binlog    int(10) unsigned NOT NULL DEFAULT '0',
-    internal_remark    varchar(255)     NOT NULL DEFAULT '',
-    internal_insert    datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    internal_update    datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    internal_delete    datetime         NULL     DEFAULT NULL,
+    id                     int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    query_id               int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
+    filter_id              int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
+    expression_code        int(10) unsigned NOT NULL DEFAULT '0' COMMENT '编码',
+    parameter_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT '子查询',
+    parameter_name         varchar(64)      NOT NULL DEFAULT '' COMMENT '参数名',
+    value_string           varchar(255)     NULL     DEFAULT NULL COMMENT '字符串',
+    value_integer          int(10)          NULL     DEFAULT NULL COMMENT '整数',
+    value_long             bigint(20)       NULL     DEFAULT NULL COMMENT '长整数',
+    value_big_decimal      decimal(14, 3)   NULL     DEFAULT NULL COMMENT '小数',
+    value_date             datetime         NULL     DEFAULT NULL COMMENT '日期时间',
+    update_version         int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
+    create_date            datetime         NULL     DEFAULT NULL COMMENT '创建时间',
+    last_update            datetime         NULL     DEFAULT NULL COMMENT '更新时间',
+    internal_recycle       int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
+    internal_binlog        int(10) unsigned NOT NULL DEFAULT '0',
+    internal_remark        varchar(255)     NOT NULL DEFAULT '',
+    internal_insert        datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    internal_update        datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    internal_delete        datetime         NULL     DEFAULT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10000
@@ -56,39 +56,39 @@ CREATE TABLE query_filter_expression
 DROP TABLE IF EXISTS query_filter_in;
 CREATE TABLE query_filter_in
 (
-    id                 int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-    query_id           int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
-    filter_id          int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
-    is_not             int(10) unsigned NOT NULL DEFAULT '0' COMMENT '取反？',
-    value_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT '子查询',
-    update_version     int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
-    create_date        datetime         NULL     DEFAULT NULL COMMENT '创建时间',
-    last_update        datetime         NULL     DEFAULT NULL COMMENT '更新时间',
-    internal_recycle   int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
-    internal_binlog    int(10) unsigned NOT NULL DEFAULT '0',
-    internal_remark    varchar(255)     NOT NULL DEFAULT '',
-    internal_insert    datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    internal_update    datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    internal_delete    datetime         NULL     DEFAULT NULL,
+    id                     int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    query_id               int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
+    filter_id              int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
+    is_not                 int(10) unsigned NOT NULL DEFAULT '0' COMMENT '取反？',
+    parameter_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT '子查询',
+    update_version         int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
+    create_date            datetime         NULL     DEFAULT NULL COMMENT '创建时间',
+    last_update            datetime         NULL     DEFAULT NULL COMMENT '更新时间',
+    internal_recycle       int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
+    internal_binlog        int(10) unsigned NOT NULL DEFAULT '0',
+    internal_remark        varchar(255)     NOT NULL DEFAULT '',
+    internal_insert        datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    internal_update        datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    internal_delete        datetime         NULL     DEFAULT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10000
-  CHARACTER SET = utf8mb4 COMMENT = '在列表中？';
+  CHARACTER SET = utf8mb4 COMMENT = '在列表中';
 
-DROP TABLE IF EXISTS query_filter_in_value;
-CREATE TABLE query_filter_in_value
+DROP TABLE IF EXISTS query_filter_in_parameter;
+CREATE TABLE query_filter_in_parameter
 (
     id                int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
     query_id          int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
     filter_id         int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
-    filter_in_id      int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Filter In Id',
-    value_name        varchar(64)      NOT NULL DEFAULT '' COMMENT 'Value Name used for REFERENCE PARAMETER',
-    value_string      varchar(255)     NULL     DEFAULT NULL COMMENT 'Value String',
-    value_integer     int(10)          NULL     DEFAULT NULL COMMENT 'Value Integer',
-    value_long        bigint(20)       NULL     DEFAULT NULL COMMENT 'Value Long',
-    value_big_decimal decimal(14, 3)   NULL     DEFAULT NULL COMMENT 'Value Big Decimal',
-    value_date        datetime         NULL     DEFAULT NULL COMMENT 'Value Date',
-    show_priority     int(10) unsigned NOT NULL DEFAULT '10000' COMMENT 'Show Priority used for ORDER BY',
+    filter_in_id      int(10) unsigned NOT NULL DEFAULT '0' COMMENT '在列表中主键',
+    parameter_name    varchar(64)      NOT NULL DEFAULT '' COMMENT '参数名',
+    value_string      varchar(255)     NULL     DEFAULT NULL COMMENT '字符串',
+    value_integer     int(10)          NULL     DEFAULT NULL COMMENT '整数',
+    value_long        bigint(20)       NULL     DEFAULT NULL COMMENT '长整数',
+    value_big_decimal decimal(14, 3)   NULL     DEFAULT NULL COMMENT '小数',
+    value_date        datetime         NULL     DEFAULT NULL COMMENT '日期时间',
+    show_priority     int(10) unsigned NOT NULL DEFAULT '10000' COMMENT '排序',
     update_version    int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
     create_date       datetime         NULL     DEFAULT NULL COMMENT '创建时间',
     last_update       datetime         NULL     DEFAULT NULL COMMENT '更新时间',
@@ -101,7 +101,7 @@ CREATE TABLE query_filter_in_value
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10000
-  CHARACTER SET = utf8mb4 COMMENT = 'Query Filter In Value';
+  CHARACTER SET = utf8mb4 COMMENT = '在列表中参数';
 
 DROP TABLE IF EXISTS query_filter_null;
 CREATE TABLE query_filter_null
@@ -122,39 +122,39 @@ CREATE TABLE query_filter_null
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10000
-  CHARACTER SET = utf8mb4 COMMENT = '空？';
+  CHARACTER SET = utf8mb4 COMMENT = '空';
 
 DROP TABLE IF EXISTS query_filter_range;
 CREATE TABLE query_filter_range
 (
-    id                      int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-    query_id                int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
-    filter_id               int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
-    is_include_lower        int(10) unsigned NOT NULL DEFAULT '0' COMMENT '包含开始？',
-    is_include_upper        int(10) unsigned NOT NULL DEFAULT '0' COMMENT '包含结束？',
-    from_value_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'From 子查询',
-    from_value_name         varchar(64)      NOT NULL DEFAULT '' COMMENT 'From Value Name used for REFERENCE PARAMETER',
-    from_value_string       varchar(255)     NULL     DEFAULT NULL COMMENT 'From Value String',
-    from_value_integer      int(10)          NULL     DEFAULT NULL COMMENT 'From Value Integer',
-    from_value_long         bigint(20)       NULL     DEFAULT NULL COMMENT 'From Value Long',
-    from_value_big_decimal  decimal(14, 3)   NULL     DEFAULT NULL COMMENT 'From Value Big Decimal',
-    from_value_date         datetime         NULL     DEFAULT NULL COMMENT 'From Value Date',
-    to_value_sub_query_id   int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'To Value Id used for Sub Query',
-    to_value_name           varchar(64)      NOT NULL DEFAULT '' COMMENT 'To Value Name used for REFERENCE PARAMETER',
-    to_value_string         varchar(255)     NULL     DEFAULT NULL COMMENT 'To Value String',
-    to_value_integer        int(10)          NULL     DEFAULT NULL COMMENT 'To Value Integer',
-    to_value_long           bigint(20)       NULL     DEFAULT NULL COMMENT 'To Value Long',
-    to_value_big_decimal    decimal(14, 3)   NULL     DEFAULT NULL COMMENT 'To Value Big Decimal',
-    to_value_date           datetime         NULL     DEFAULT NULL COMMENT 'To Value Date',
-    update_version          int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
-    create_date             datetime         NULL     DEFAULT NULL COMMENT '创建时间',
-    last_update             datetime         NULL     DEFAULT NULL COMMENT '更新时间',
-    internal_recycle        int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
-    internal_binlog         int(10) unsigned NOT NULL DEFAULT '0',
-    internal_remark         varchar(255)     NOT NULL DEFAULT '',
-    internal_insert         datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    internal_update         datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    internal_delete         datetime         NULL     DEFAULT NULL,
+    id                          int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    query_id                    int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
+    filter_id                   int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
+    is_include_lower            int(10) unsigned NOT NULL DEFAULT '0' COMMENT '包含开始？',
+    is_include_upper            int(10) unsigned NOT NULL DEFAULT '0' COMMENT '包含结束？',
+    from_parameter_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT '开始子查询',
+    from_parameter_name         varchar(64)      NOT NULL DEFAULT '' COMMENT '开始参数名',
+    from_value_string           varchar(255)     NULL     DEFAULT NULL COMMENT '开始字符串',
+    from_value_integer          int(10)          NULL     DEFAULT NULL COMMENT '开始整数',
+    from_value_long             bigint(20)       NULL     DEFAULT NULL COMMENT '开始长整数',
+    from_value_big_decimal      decimal(14, 3)   NULL     DEFAULT NULL COMMENT '开始小数',
+    from_value_date             datetime         NULL     DEFAULT NULL COMMENT '开始日期时间',
+    to_parameter_sub_query_id   int(10) unsigned NOT NULL DEFAULT '0' COMMENT '结束子查询',
+    to_parameter_name           varchar(64)      NOT NULL DEFAULT '' COMMENT '结束参数名',
+    to_value_string             varchar(255)     NULL     DEFAULT NULL COMMENT '结束字符串',
+    to_value_integer            int(10)          NULL     DEFAULT NULL COMMENT '结束整数',
+    to_value_long               bigint(20)       NULL     DEFAULT NULL COMMENT '结束长整数',
+    to_value_big_decimal        decimal(14, 3)   NULL     DEFAULT NULL COMMENT '结束小数',
+    to_value_date               datetime         NULL     DEFAULT NULL COMMENT '结束日期时间',
+    update_version              int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
+    create_date                 datetime         NULL     DEFAULT NULL COMMENT '创建时间',
+    last_update                 datetime         NULL     DEFAULT NULL COMMENT '更新时间',
+    internal_recycle            int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
+    internal_binlog             int(10) unsigned NOT NULL DEFAULT '0',
+    internal_remark             varchar(255)     NOT NULL DEFAULT '',
+    internal_insert             datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    internal_update             datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    internal_delete             datetime         NULL     DEFAULT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10000
@@ -163,22 +163,27 @@ CREATE TABLE query_filter_range
 DROP TABLE IF EXISTS query_filter_wildcard;
 CREATE TABLE query_filter_wildcard
 (
-    id               int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-    query_id         int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
-    filter_id        int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
-    is_not           int(10) unsigned NOT NULL DEFAULT '0' COMMENT '取反？',
-    wildcard_code    int(10) unsigned NOT NULL DEFAULT '0' COMMENT '编码',
-    value_name       varchar(64)      NOT NULL DEFAULT '' COMMENT 'Value Name used for REFERENCE PARAMETER',
-    value_string     varchar(255)     NULL     DEFAULT NULL COMMENT 'Value String',
-    update_version   int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
-    create_date      datetime         NULL     DEFAULT NULL COMMENT '创建时间',
-    last_update      datetime         NULL     DEFAULT NULL COMMENT '更新时间',
-    internal_recycle int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
-    internal_binlog  int(10) unsigned NOT NULL DEFAULT '0',
-    internal_remark  varchar(255)     NOT NULL DEFAULT '',
-    internal_insert  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    internal_update  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    internal_delete  datetime         NULL     DEFAULT NULL,
+    id                     int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    query_id               int(10) unsigned NOT NULL DEFAULT '0' COMMENT '查询主键',
+    filter_id              int(10) unsigned NOT NULL DEFAULT '0' COMMENT '条件主键',
+    is_not                 int(10) unsigned NOT NULL DEFAULT '0' COMMENT '取反？',
+    wildcard_code          int(10) unsigned NOT NULL DEFAULT '0' COMMENT '编码',
+    parameter_sub_query_id int(10) unsigned NOT NULL DEFAULT '0' COMMENT '子查询',
+    parameter_name         varchar(64)      NOT NULL DEFAULT '' COMMENT '参数名',
+    value_string           varchar(255)     NULL     DEFAULT NULL COMMENT '字符串',
+    value_integer          int(10)          NULL     DEFAULT NULL COMMENT '整数',
+    value_long             bigint(20)       NULL     DEFAULT NULL COMMENT '长整数',
+    value_big_decimal      decimal(14, 3)   NULL     DEFAULT NULL COMMENT '小数',
+    value_date             datetime         NULL     DEFAULT NULL COMMENT '日期时间',
+    update_version         int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新版本',
+    create_date            datetime         NULL     DEFAULT NULL COMMENT '创建时间',
+    last_update            datetime         NULL     DEFAULT NULL COMMENT '更新时间',
+    internal_recycle       int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Recycled ? id : 0',
+    internal_binlog        int(10) unsigned NOT NULL DEFAULT '0',
+    internal_remark        varchar(255)     NOT NULL DEFAULT '',
+    internal_insert        datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    internal_update        datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    internal_delete        datetime         NULL     DEFAULT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 10000
