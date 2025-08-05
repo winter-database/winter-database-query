1f049b6558d0ab3f87aacc9a5def47cc6c4cfa06
changebooks <changebooks@qq.com>
2025-08-05 23:29:24 +0800

    2

---
+++ src/main/java/io/github/winter/database/query/QueryFilter.java
@@ -0,0 +1,12 @@
+package io.github.winter.database.query;
+
+/**
+ * 条件
+ *
+ * @author changebooks@qq.com
+ */
+public final class QueryFilter {
+
+
+
+}
---
+++ src/main/java/io/github/winter/database/query/SubQuery.java
@@ -0,0 +1,14 @@
+package io.github.winter.database.query;
+
+import java.io.Serializable;
+
+/**
+ * 子查询
+ *
+ * @author changebooks@qq.com
+ */
+public final class SubQuery implements Serializable {
+
+
+
+}
