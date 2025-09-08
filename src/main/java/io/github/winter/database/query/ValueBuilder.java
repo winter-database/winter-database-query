1446b07226eb466dd21bd75e42d4444c48fe3529
changebooks <changebooks@qq.com>
2025-09-08 23:38:47 +0800

    1

---
+++ src/main/java/io/github/winter/database/query/ValueBuilder.java
@@ -0,0 +1,58 @@
+package io.github.winter.database.query;
+
+import io.github.winter.boot.tuple.Value;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Value Builder
+ *
+ * @author changebooks@qq.com
+ */
+public final class ValueBuilder {
+
+    private ValueBuilder() {
+    }
+
+    /**
+     * Build
+     *
+     * @param valueType       Value Type
+     * @param valueString     String Value
+     * @param valueInteger    Integer Value
+     * @param valueLong       Long Value
+     * @param valueBigDecimal BigDecimal Value
+     * @param valueDate       Date Value
+     * @return the {@link Value} instance
+     */
+    public static Value build(Class<?> valueType,
+                              String valueString, Integer valueInteger, Long valueLong, BigDecimal valueBigDecimal, Date valueDate) {
+        if (valueType == null) {
+            return null;
+        }
+
+        if (String.class == valueType) {
+            return new Value(valueString);
+        }
+
+        if (Integer.class == valueType) {
+            return new Value(valueInteger);
+        }
+
+        if (Long.class == valueType) {
+            return new Value(valueLong);
+        }
+
+        if (BigDecimal.class == valueType) {
+            return new Value(valueBigDecimal);
+        }
+
+        if (Date.class == valueType) {
+            return new Value(valueDate);
+        }
+
+        return null;
+    }
+
+}
---
+++ src/main/java/io/github/winter/database/query/ValueTypeGetter.java
@@ -0,0 +1,47 @@
+package io.github.winter.database.query;
+
+import io.github.winter.database.table.TableSchema;
+import io.github.winter.database.template.TableSchemaRegistry;
+
+import java.util.Map;
+
+/**
+ * Get Value Type
+ *
+ * @author changebooks@qq.com
+ */
+public final class ValueTypeGetter {
+
+    private ValueTypeGetter() {
+    }
+
+    /**
+     * Get Value Type
+     *
+     * @param tableName  Table Name
+     * @param columnName Column Name
+     * @return Value Type
+     */
+    public static Class<?> getValueType(String tableName, String columnName) {
+        if (tableName == null || tableName.isEmpty()) {
+            return null;
+        }
+
+        if (columnName == null || columnName.isEmpty()) {
+            return null;
+        }
+
+        TableSchema tableSchema = TableSchemaRegistry.get(tableName);
+        if (tableSchema == null) {
+            return null;
+        }
+
+        Map<String, Class<?>> valueTypes = tableSchema.getValueTypes();
+        if (valueTypes == null) {
+            return null;
+        } else {
+            return valueTypes.get(columnName);
+        }
+    }
+
+}
