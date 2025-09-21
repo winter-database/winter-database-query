b80a42e839edc4c435748fcd1363cb1d14486659
changebooks <changebooks@qq.com>
2025-09-21 12:43:28 +0800

    1

---
+++ src/main/java/io/github/winter/database/query/ColumnBuilder.java
@@ -0,0 +1,91 @@
+package io.github.winter.database.query;
+
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * 创建字段
+ *
+ * @author changebooks@qq.com
+ */
+public interface ColumnBuilder {
+    /**
+     * 创建
+     *
+     * @param funcType   函数类型
+     * @param tableName  表名
+     * @param columnName 字段名
+     * @return the {@link Column} instance
+     */
+    @NotNull
+    Column build(int funcType, String tableName, String columnName);
+
+    /**
+     * 连接别名
+     *
+     * @param fieldName 函数 + 表名 + 字段名
+     * @param asName    别名
+     * @return 函数 + 表名 + 字段名 + 别名
+     */
+    @NotNull
+    String joinAs(String fieldName, String asName);
+
+    /**
+     * 连接函数
+     *
+     * @param funcType   函数类型
+     * @param tableName  表名
+     * @param columnName 字段名
+     * @return 函数 + 表名 + 字段名
+     */
+    @NotNull
+    String joinFunc(int funcType, String tableName, String columnName);
+
+    /**
+     * 连接函数
+     *
+     * @param funcType  函数类型
+     * @param fieldName 表名 + 字段名
+     * @return 函数 + 表名 + 字段名
+     */
+    @NotNull
+    String joinFunc(int funcType, String fieldName);
+
+    /**
+     * 连接表名
+     *
+     * @param tableName  表名
+     * @param columnName 字段名
+     * @return 表名 + 字段名
+     */
+    @NotNull
+    String joinName(String tableName, String columnName);
+
+    /**
+     * 解析别名
+     *
+     * @param fieldName 函数 + 表名 + 字段名
+     * @return 别名
+     */
+    @NotNull
+    String asName(String fieldName);
+
+    /**
+     * 解析类型
+     *
+     * @param funcType   函数类型
+     * @param tableName  表名
+     * @param columnName 字段名
+     * @return 字段类型
+     */
+    Class<?> parseType(int funcType, String tableName, String columnName);
+
+    /**
+     * 解析类型
+     *
+     * @param tableName  表名
+     * @param columnName 字段名
+     * @return 字段类型
+     */
+    Class<?> parseType(String tableName, String columnName);
+
+}
---
+++ src/main/java/io/github/winter/database/query/builder/ColumnBuilderImpl.java
@@ -0,0 +1,122 @@
+package io.github.winter.database.query.builder;
+
+import io.github.winter.database.query.Column;
+import io.github.winter.database.query.ColumnBuilder;
+import io.github.winter.database.query.FuncType;
+import io.github.winter.database.table.TableSchema;
+import io.github.winter.database.template.TableSchemaRegistry;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ * @author changebooks@qq.com
+ */
+public class ColumnBuilderImpl implements ColumnBuilder {
+
+    @Override
+    public Column build(int funcType, String tableName, String columnName) {
+        String fieldName = joinFunc(funcType, tableName, columnName);
+        String asName = asName(fieldName);
+        String sqlName = joinAs(fieldName, asName);
+        Class<?> valueType = parseType(funcType, tableName, columnName);
+
+        Column result = new Column();
+
+        result.setSqlName(sqlName);
+        result.setFieldName(fieldName);
+        result.setAsName(asName);
+        result.setFuncType(funcType);
+        result.setTableName(tableName);
+        result.setColumnName(columnName);
+        result.setValueType(valueType);
+
+        return result;
+    }
+
+    @Override
+    public String joinAs(String fieldName, String asName) {
+        if (fieldName == null || fieldName.isEmpty()) {
+            return "";
+        }
+
+        if (asName == null || asName.isEmpty()) {
+            return "";
+        } else {
+            return fieldName + " AS `" + asName + "`";
+        }
+    }
+
+    @Override
+    public String joinFunc(int funcType, String tableName, String columnName) {
+        String fieldName = joinName(tableName, columnName);
+        return joinFunc(funcType, fieldName);
+    }
+
+    @Override
+    public String joinFunc(int funcType, String fieldName) {
+        return switch (funcType) {
+            case FuncType.COUNT -> "COUNT(*)";
+            case FuncType.SUM -> "SUM(" + fieldName + ")";
+            case FuncType.MAX -> "MAX(" + fieldName + ")";
+            case FuncType.MIN -> "MIN(" + fieldName + ")";
+            case FuncType.AVG -> "AVG(" + fieldName + ")";
+            default -> fieldName;
+        };
+    }
+
+    @Override
+    public String joinName(String tableName, String columnName) {
+        if (columnName == null || columnName.isEmpty()) {
+            return "";
+        }
+
+        if (tableName == null || tableName.isEmpty()) {
+            return columnName;
+        } else {
+            return tableName + '.' + columnName;
+        }
+    }
+
+    @Override
+    public String asName(String fieldName) {
+        if (fieldName == null || fieldName.isEmpty()) {
+            return "";
+        } else {
+            return fieldName;
+        }
+    }
+
+    @Override
+    public Class<?> parseType(int funcType, String tableName, String columnName) {
+        return switch (funcType) {
+            case FuncType.COUNT, FuncType.SUM -> Long.class;
+            case FuncType.AVG -> BigDecimal.class;
+            default -> parseType(tableName, columnName);
+        };
+    }
+
+    @Override
+    public Class<?> parseType(String tableName, String columnName) {
+        if (tableName == null || tableName.isEmpty()) {
+            return null;
+        }
+
+        if (columnName == null || columnName.isEmpty()) {
+            return null;
+        }
+
+        TableSchema tableSchema = TableSchemaRegistry.get(tableName);
+        if (tableSchema == null) {
+            return null;
+        }
+
+        Map<String, Class<?>> valueTypes = tableSchema.getValueTypes();
+        if (valueTypes == null) {
+            return null;
+        } else {
+            return valueTypes.get(columnName);
+        }
+    }
+
+}
