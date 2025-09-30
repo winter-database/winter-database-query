b69d3338b891b0065984b81644b90713d07333a3
changebooks <changebooks@qq.com>
2025-09-30 21:27:43 +0800

    Update Query.java

--- src/main/java/io/github/winter/database/query/entity/Query.java
+++ src/main/java/io/github/winter/database/query/entity/Query.java
@@ -1,8 +1,13 @@
 package io.github.winter.database.query.entity;
 
+import io.github.winter.boot.tuple.Value;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * 查询
@@ -60,8 +65,69 @@ public final class Query implements Serializable {
      */
     private String queryRemark;
 
+    /**
+     * Build Instance List
+     *
+     * @param list [ [ Column Name : Column Value ] ]
+     * @return [ the {@link Query} instance ]
+     */
+    public static List<Query> newInstance(List<Map<String, Value>> list) {
+        if (list != null) {
+            return list.stream()
+                    .filter(Objects::nonNull)
+                    .map(Query::newInstance)
+                    .filter(Objects::nonNull)
+                    .toList();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Build Instance
+     *
+     * @param record [ Column Name : Column Value ]
+     * @return the {@link Query} instance
+     */
+    public static Query newInstance(Map<String, Value> record) {
+        if (record == null) {
+            return null;
+        }
+
+        Value id = record.get("id");
+        Value queryName = record.get("query_name");
+        Value isDistinct = record.get("is_distinct");
+        Value isAsterisk = record.get("is_asterisk");
+        Value isParameterName = record.get("is_parameter_name");
+        Value fromTable = record.get("from_table");
+        Value pageOffset = record.get("page_offset");
+        Value pageLimit = record.get("page_limit");
+        Value queryDescription = record.get("query_description");
+        Value queryRemark = record.get("query_remark");
+
+        Query result = new Query();
+
+        result.setId(id);
+        result.setQueryName(queryName);
+        result.setDistinct(isDistinct);
+        result.setAsterisk(isAsterisk);
+        result.setParameterName(isParameterName);
+        result.setFromTable(fromTable);
+        result.setPageOffset(pageOffset);
+        result.setPageLimit(pageLimit);
+        result.setQueryDescription(queryDescription);
+        result.setQueryRemark(queryRemark);
+
+        return result;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public void setId(Value value) {
+        int id = Optional.ofNullable(value).map(Value::getInteger).orElse(0);
+        setId(id);
     }
 
     public void setId(int id) {
@@ -73,12 +139,23 @@ public final class Query implements Serializable {
         return queryName != null ? queryName : "";
     }
 
+    public void setQueryName(Value value) {
+        String queryName = value != null ? value.getString() : null;
+        setQueryName(queryName);
+    }
+
     public void setQueryName(String queryName) {
         this.queryName = queryName != null ? queryName.trim() : "";
     }
 
     public boolean isDistinct() {
         return distinct;
+    }
+
+    public void setDistinct(Value value) {
+        Integer distinct = value != null ? value.getInteger() : null;
+        boolean isDistinct = BooleanCast.fromInt(distinct);
+        setDistinct(isDistinct);
     }
 
     public void setDistinct(boolean distinct) {
@@ -89,12 +166,24 @@ public final class Query implements Serializable {
         return asterisk;
     }
 
+    public void setAsterisk(Value value) {
+        Integer asterisk = value != null ? value.getInteger() : null;
+        boolean isAsterisk = BooleanCast.fromInt(asterisk);
+        setAsterisk(isAsterisk);
+    }
+
     public void setAsterisk(boolean asterisk) {
         this.asterisk = asterisk;
     }
 
     public boolean isParameterName() {
         return parameterName;
+    }
+
+    public void setParameterName(Value value) {
+        Integer parameterName = value != null ? value.getInteger() : null;
+        boolean isParameterName = BooleanCast.fromInt(parameterName);
+        setParameterName(isParameterName);
     }
 
     public void setParameterName(boolean parameterName) {
@@ -106,12 +195,22 @@ public final class Query implements Serializable {
         return fromTable != null ? fromTable : "";
     }
 
+    public void setFromTable(Value value) {
+        String fromTable = value != null ? value.getString() : null;
+        setFromTable(fromTable);
+    }
+
     public void setFromTable(String fromTable) {
         this.fromTable = fromTable != null ? fromTable.trim() : "";
     }
 
     public long getPageOffset() {
         return pageOffset;
+    }
+
+    public void setPageOffset(Value value) {
+        long pageOffset = Optional.ofNullable(value).map(Value::getLong).orElse(0L);
+        setPageOffset(pageOffset);
     }
 
     public void setPageOffset(long pageOffset) {
@@ -122,24 +221,41 @@ public final class Query implements Serializable {
         return pageLimit;
     }
 
+    public void setPageLimit(Value value) {
+        int pageLimit = Optional.ofNullable(value).map(Value::getInteger).orElse(0);
+        setPageLimit(pageLimit);
+    }
+
     public void setPageLimit(int pageLimit) {
         this.pageLimit = pageLimit;
     }
 
+    @NotNull
     public String getQueryDescription() {
-        return queryDescription;
+        return queryDescription != null ? queryDescription : "";
+    }
+
+    public void setQueryDescription(Value value) {
+        String queryDescription = value != null ? value.getString() : null;
+        setQueryDescription(queryDescription);
     }
 
     public void setQueryDescription(String queryDescription) {
-        this.queryDescription = queryDescription;
+        this.queryDescription = queryDescription != null ? queryDescription.trim() : "";
     }
 
+    @NotNull
     public String getQueryRemark() {
-        return queryRemark;
+        return queryRemark != null ? queryRemark : "";
+    }
+
+    public void setQueryRemark(Value value) {
+        String queryRemark = value != null ? value.getString() : null;
+        setQueryRemark(queryRemark);
     }
 
     public void setQueryRemark(String queryRemark) {
-        this.queryRemark = queryRemark;
+        this.queryRemark = queryRemark != null ? queryRemark.trim() : "";
     }
 
 }
